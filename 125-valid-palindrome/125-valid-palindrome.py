7374acd8216793057e23e28de3e4d73cb206839e
Aniket Kamble <96435498+Aniket-404@users.noreply.github.com>
2025-11-02 14:31:27 +0530

    Add 125. Valid Palindrome solution

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_str = ''.join(c for c in s if c.isalnum()).lower()
+        str_rev = new_str[::-1]
+        if new_str == str_rev:
+            return True
+        else:
+            return False
