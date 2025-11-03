75261ebe32618145a4ad8f82e3f4dab5bf77db54
Aniket Kamble <96435498+Aniket-404@users.noreply.github.com>
2025-11-03 14:53:01 +0530

    Update 196. Delete Duplicate Emails solution

--- 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -1,10 +1,8 @@
-# Write your MySQL query statement below
 DELETE FROM Person
 WHERE id NOT IN (
-SELECT id FROM 
- (  
-   SELECT MIN(id) AS id 
-   FROM Person 
-   GROUP BY email
-   ) As tmp 
-)
+    SELECT keeper_id FROM (
+        SELECT MIN(id) AS keeper_id
+        FROM Person
+        GROUP BY email
+    ) AS temp_table
+);
