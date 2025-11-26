a9c3bb402da4e49fd8219fee718d9f85baeced78
Aniket Kamble <96435498+Aniket-404@users.noreply.github.com>
2025-11-26 16:03:46 +0530

    Update 242. Valid Anagram solution

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -4,7 +4,13 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        s_count = Counter(s)
-        t_count = Counter(t)
+        #s_count = Counter(s)
+        #t_count = Counter(t)
+        s_map = {}
+        t_map = {}
+        for char in s:
+            s_map[char] = s_map.get(char, 0) + 1
+        for char in t:
+            t_map[char] = t_map.get(char, 0) + 1
 
-        return s_count == t_count
+        return s_map == t_map
